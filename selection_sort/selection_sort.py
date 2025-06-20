d2acdfc99d04fb9d3f8da4b59c88d0eb87444acf
natasha kemunto maria <160590292+ntsh-o5@users.noreply.github.com>
2025-06-20 14:14:00 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,9 @@
-def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+numbers = [5, 4, 3, 2, 1, 0]
+
+for i in range(len(numbers) - 1):
+    for j in range(i + 1, len(numbers)):
+        if numbers[j] < numbers[min_index]:
+            min_index = j
+    numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+    
+print("Sorted list:", numbers)
