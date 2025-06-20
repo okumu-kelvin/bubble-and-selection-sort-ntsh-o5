a8086d0f5cd367536eea1ac431ba24a6c2dc2508
natasha kemunto maria <160590292+ntsh-o5@users.noreply.github.com>
2025-06-20 14:13:30 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+numbers = [5, 4, 3, 2, 1, 0]
+
+n = len(numbers)
+
+for i in range(n - 1):  # Outer loop: runs n-1 times. On each pass, the biggest number "bubbles" to the end.
+    for j in range(n - 1 - i):  # Inner loop: avoids already sorted part at the end.
+        if numbers[j] > numbers[j + 1]:
+            temp = numbers[j]
+            numbers[j] = numbers[j + 1]
+            numbers[j + 1] = temp
+
+print(numbers)
+
